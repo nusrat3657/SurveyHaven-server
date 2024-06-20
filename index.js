2ff41dcc9db4a3942ac6f80edc6b532ec205bd77
nusrat3657 <nusratjahan3657r@gmail.com>
2024-06-20 23:21:31 +0600

    added usercollection and updated users

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hsfxbe1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,6 +45,24 @@ async function run() {
 
         app.get('/users', async(req,res) => {
             const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async(req,res) => {
+            const filter = {_id: new ObjectId(id)};
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        } )
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
